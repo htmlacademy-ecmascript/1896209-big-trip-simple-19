e22f56a6273a4f61775b417d0eded7be3f0876aa
Валерия <lerusik434@gmail.com>
2023-02-07 10:29:44 +0400

    ERROR

--- src/view/creation-form.js
+++ src/view/creation-form.js
@@ -58,7 +58,7 @@ function createCreationFormTemplate (point) {
         <label class="event__label  event__type-output" for="event-destination-1">
         ${type}
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${getDestination(point).name}" list="destination-list-1">
+        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="" list="destination-list-1">
         <datalist id="destination-list-1">
 
         </datalist>
